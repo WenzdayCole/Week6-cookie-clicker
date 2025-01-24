57f397a3c8eebe6b7e1d8a824c72433467df02bc
Ricards Rudups <ricardsrudups99@gmail.com>
2025-01-24 16:09:45 +0000

    pps timer working, not rendered yet.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CookieCount from './components/CookieCount';
+
+export default function App() {
+
+  return (
+    <>
+    <h1>
+      Poke Clicker
+    </h1>
+    <CookieCount/>
+    </>
+  );
+}
---
+++ src/components/CookieCount.jsx
@@ -0,0 +1,50 @@
+// import React, { useState, useEffect } from "react";
+
+// export default function CookieCount() {
+//     console.log("CookieCount component render");
+//     const [count, setCount] = useState(0)
+//     const [cps, setCps] = useState(1)
+
+//     useEffect(() => {
+//         console.log("CookieCount component useEffect call back");
+//         const myInterval = setInterval(() => {
+//             setCps((currentCookies) => {
+//                 currentCookies + cps;
+//             });
+//         }, 1000);
+//     }, []);
+
+//         return () => {
+//             clearInterval(myInterval);
+//         };
+  
+// }
+
+
+import React, { useState, useEffect } from "react";
+
+export default function CookieCount(props) {
+  console.log("CookieCount component render");
+
+  const [count, setCount] = useState(0); 
+  const [cps, setCps] = useState(1);
+
+  useEffect(() => {
+    console.log("CookieCount component useEffect callback");
+const myInterval = setInterval(() => {
+      setCount((currentCookies) => currentCookies + cps);
+    }, 1000);
+return () => {
+      clearInterval(myInterval);
+    };
+  }, [cps])
+
+
+return (
+    <>
+    {/* <p>Pokemon per second:</p>  do something with the cps  */}
+    </>
+)
+
+
+};
